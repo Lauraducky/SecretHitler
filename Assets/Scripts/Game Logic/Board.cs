d8b20fa4e09632b735846f43ecdf97aab0b6777e
Callum Grier <cal.grier@gmail.com>
2016-01-22 21:14:15 +1000

    Worked more on game logic, and started on game loop

---
+++ Assets/Scripts/Game Logic/Board.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using System.Threading;
+
+namespace SecretHitler {
+    public static class Boards {
+        public static Board[] FascistBoards;
+        public static Board LiberalBoard;
+
+        public static void SetUp () {
+            FascistBoards = new Board[3];
+            FascistBoards[0] = new Board(6);
+                FascistBoards[0].SetPower(3, Board.POWERS.PolicyCheck);
+                FascistBoards[0].SetPower(4, Board.POWERS.Assassination);
+                FascistBoards[0].SetPower(5, Board.POWERS.Assassination);
+            FascistBoards[1] = new Board(6);
+                FascistBoards[1].SetPower(2, Board.POWERS.LoyaltyCheck);
+                FascistBoards[1].SetPower(3, Board.POWERS.SpecialElection);
+                FascistBoards[1].SetPower(4, Board.POWERS.Assassination);
+                FascistBoards[1].SetPower(5, Board.POWERS.Assassination);
+            FascistBoards[2] = new Board(6);
+                FascistBoards[2].SetPower(1, Board.POWERS.LoyaltyCheck);
+                FascistBoards[2].SetPower(2, Board.POWERS.LoyaltyCheck);
+                FascistBoards[2].SetPower(3, Board.POWERS.SpecialElection);
+                FascistBoards[2].SetPower(4, Board.POWERS.Assassination);
+                FascistBoards[2].SetPower(5, Board.POWERS.Assassination);
+            LiberalBoard = new Board(5);
+        }
+
+        public static void ClearAll() {
+            foreach (Board b in FascistBoards)
+                b.CardsPlayed = 0;
+            LiberalBoard.CardsPlayed = 0;
+        }
+    }
+
+    public class Board
+    {
+        public enum POWERS { None, PolicyCheck, SpecialElection, LoyaltyCheck, Assassination };
+        private POWERS[] spaces;
+        private int cardsPlayed;
+
+        public Board(int boardLength) {
+            spaces = new POWERS[boardLength];
+            for (int i = 0; i < boardLength; i++)
+                spaces[i] = POWERS.None;
+            cardsPlayed = 0;
+        }
+        
+        public void SetPower (int position, POWERS power) {
+            spaces[position-1] = power;
+        }
+
+        public int CardsPlayed {
+            get {
+                return cardsPlayed;
+            }
+            set {
+                cardsPlayed = value;
+            }
+        }
+    }
+}
--- Assets/Scripts/Game Logic/PlayerLogic.cs
+++ Assets/Scripts/Game Logic/PlayerLogic.cs
@@ -9,22 +9,42 @@ namespace SecretHitler {
     /// Class for receiving information from the server/gamemanager and for the GUI to recieve
     /// </summary>
     public class PlayerLogic {
+        private string name;
+        private int id;
+
         public enum ROLES { Liberal, Fascist, Hitler };
         private ROLES role;
         
         public enum PLAYERSTATES { Idle, Voting, SelectingChance, PresPolicy, ChanPolicy, Investigate, SpecialElection, PolicyPeek, Assassinate };
         private PLAYERSTATES playerState;
 
+        private List<bool> hand;
         private bool isPres;
         private bool isChan;
-        private List<bool> hand;
+        private bool isDead;
+        private bool hasVoted;
+        private bool vote;
 
-        public PlayerLogic() {
-            hand = new List<bool>();
-            role = ROLES.Liberal;
-            playerState = PLAYERSTATES.Idle;
-            isPres = false;
-            isChan = false;
+        public PlayerLogic(int id, string name) {
+            this.name = name;
+            this.id = id;
+            this.hand = new List<bool>();
+            this.role = ROLES.Liberal;
+            this.playerState = PLAYERSTATES.Idle;
+            this.isPres = false;
+            this.isChan = false;
+        }
+
+        public int ID {
+            get {
+                return id;
+            }
+        }
+
+        public string Name {
+            get {
+                return name;
+            }
         }
 
         public ROLES Role {
@@ -63,13 +83,33 @@ namespace SecretHitler {
             }
         }
 
-        public void AddToHand (bool card) {
-            hand.Add(card);
+        public List<bool> Hand {
+            get {
+                return hand;
+            }
+            set {
+                hand = value;
+            }
         }
 
-        private void DiscardCard (bool b) {
-            hand.Remove(b);
-            GameManager.policies.discard(b);
+        public bool HasVoted {
+            get {
+                return hasVoted;
+            }
+            set {
+                if(value == false)
+                    hasVoted = value;
+            }
+        }
+
+        public bool Vote {
+            get {
+                return vote;
+            }
+            set {
+                hasVoted = true;
+                vote = value;
+            }
         }
     }
 }
